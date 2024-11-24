337ce32a566808645c0cbcf4ddfb5182019dd435
4n483l <anabel.notar@gmail.com>
2024-11-24 21:46:19 +0100

    archivo rutas

--- src/app/config/api-routes.ts
+++ src/app/config/api-routes.ts
@@ -3,12 +3,12 @@ const API_URL = 'http://127.0.0.1:8000/api';
 
 export const API_ROUTES = {
   auth: API_URL,
-  concerts: '${API_URL}/concerts',
-  rehearsals: '${API_URL}/rehearsals',
-  subjects: '${API_URL}/subjects',
-  instruments: '${API_URL}/subjects/instruments',
-  tuitions: '${API_URL}/tuitions',
-  exams: '${API_URL}/exams',
-  courses: '${API_URL}/courses',
-  notes: '${API_URL}/notes',
+  concerts: `${API_URL}/concerts`,
+  rehearsals: `${API_URL}/rehearsals`,
+  subjects: `${API_URL}/subjects`,
+  instruments: `${API_URL}/subjects/instruments`,
+  tuitions: `${API_URL}/tuitions`,
+  exams: `${API_URL}/exams`,
+  courses: `${API_URL}/courses`,
+  notes: `${API_URL}/notes`,
 };
